d72e3f62b453760f447242dac3cd368a60c3ad6f
Chris Watts <cwatts@simplymeasured.com>
2016-09-21 22:16:03 -0700

    Add update command

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* jshint node: true */
 'use strict';
+const commands = require('./lib/commands');
 
 module.exports = {
-  name: 'ember-cli-commands'
+  name: 'ember-cli-commands',
+  isDevelopingAddon() {
+    return true;
+  },
+  includedCommands() {
+    return commands;
+  },
 };
